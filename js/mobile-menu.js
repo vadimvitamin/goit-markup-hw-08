51a03909bf6d1a8c6142c29954739e2373e33b03
Vadym Kolybchuk <99030890+vadimvitamin@users.noreply.github.com>
2022-07-17 13:52:01 +0300

    qwerty

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,28 @@
 (() => {
-  const refs = {
-    openMenuBtn: document.querySelector(".data-menu-button"),
-    closeMenuBtn: document.querySelector(".open"),
-    menu: document.querySelector(".close"),
-    body: document.querySelector("body"),
+  const mobileMenu = document.querySelector(".js-menu-container");
+  const openMenuBtn = document.querySelector(".js-open-menu");
+  const closeMenuBtn = document.querySelector(".js-close-menu");
+  const body = document.querySelector(".page-body");
+
+  console.log(closeMenuBtn);
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute("aria-expanded") === "true" || false;
+    openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
+    mobileMenu.classList.toggle("is-open");
+
+    body.classList.toggle("no-scroll");
   };
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
-  function toggleMenu() {
-    refs.menu.classList.toggle("is-hidden");
-    refs.body.classList.toggle("no-scroll");
-  }
+
+  openMenuBtn.addEventListener("click", toggleMenu);
+  closeMenuBtn.addEventListener("click", toggleMenu);
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove("is-open");
+    openMenuBtn.setAttribute("aria-expanded", false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
 })();

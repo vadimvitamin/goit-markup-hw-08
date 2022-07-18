2b2b5c837cf31639609d761a00be643083082fec
Vadym Kolybchuk <99030890+vadimvitamin@users.noreply.github.com>
2022-07-18 16:18:19 +0300

    qwerty

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -11,14 +11,12 @@
       openMenuBtn.getAttribute("aria-expanded") === "true" || false;
     openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
     mobileMenu.classList.toggle("is-open");
-
     body.classList.toggle("no-scroll");
   };
 
   openMenuBtn.addEventListener("click", toggleMenu);
   closeMenuBtn.addEventListener("click", toggleMenu);
 
-  // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
     if (!e.matches) return;
     mobileMenu.classList.remove("is-open");
